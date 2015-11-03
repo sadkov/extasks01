e9ac3a44e4d1153cfe3cb65ea51343d25515eaf8
Vladimir Sadkov <sadkov@gmail.com>
2015-11-03 15:24:26 +0200

    update ex05-1.sh: tested, working

--- ex05-1.sh
+++ ex05-1.sh
@@ -5,13 +5,16 @@ for object in $@
 do
 	object=`echo "$object" | tr "[:lower:]" "[:upper:]"`
 
-	ls $object >/dev/null	
-
-    if [ $exitcode -ne 0 ]
-    then
-        echo "object $object not found in current directory"
-    else
-	echo "object $object FOUND in current directory"
-    fi
+	#working redirection patterns
+	#ls $object 2>/dev/null 1>/dev/null
+	#ls $object >/dev/null 2>&1
+	ls $object 2>/dev/null >/dev/null
+	exitcode=$?
+    	if [ $exitcode -ne 0 ]
+    	then
+   	     echo "$0: object \"$object\" not found in current directory"
+    	else
+	     echo "$0: object \"$object\" FOUND in current directory"
+    	fi
 
 done
